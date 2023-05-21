329ae2c94e322b973f1f13825ee7df85dfed3dd8
SergiiMytrofanov <122700399+SergiiMytrofanov@users.noreply.github.com>
2023-05-21 22:46:46 +0300

    Update task-09.js

--- js/task-09.js
+++ js/task-09.js
@@ -1,3 +1,13 @@
+const colorChange = document.querySelector('change-color');
+const spanColor = document.querySelector('color');
+
+colorChange.addEventListener('click', () => {
+  const randomColor = getRandomHexColor();
+  document.body.style.backgroundColor = randomColor;
+  spanColor.textContent = randomColor;
+})
+
+
 function getRandomHexColor() {
   return `#${Math.floor(Math.random() * 16777215)
     .toString(16)
